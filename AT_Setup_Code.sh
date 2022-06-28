7586959ac2ffc70ec7deab9f745eb507e4993e3f
mcreelma <mitchcreelman@gmail.com>
2022-06-28 12:13:54 -0600

    initial commit

---
+++ AT_Setup_Code.sh
@@ -0,0 +1,6 @@
+conda create -n AT python=3.8 -y
+conda activate AT
+conda install -n landsat ipykernel --update-deps --force-reinstall -y
+conda install -c anaconda scikit-learn -y
+conda install -c conda-forge kaggle -y 
+conda install -c anaconda pandas -y
