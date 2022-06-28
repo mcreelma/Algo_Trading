8fc703d7ac4fcbd45587729752bf2bd333c4c7cd
mcreelma <mitchcreelman@gmail.com>
2022-06-28 15:57:02 -0600

    starting the code

--- AT_Setup_Code.sh
+++ AT_Setup_Code.sh
@@ -4,3 +4,4 @@ conda install -n landsat ipykernel --update-deps --force-reinstall -y
 conda install -c anaconda scikit-learn -y
 conda install -c conda-forge kaggle -y 
 conda install -c anaconda pandas -y
+conda install -c conda-forge ipython-sql -y
